92663517b0e9d8f4649b8b599941baa72559d704
Olena <elenasergeevvnna@gmail.com>
2022-09-05 23:46:18 +0300

    Created Json models.

---
+++ JSON + REST API Practice/Models/JsonModels.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  JSON + REST API Practice
+//
+//  Created by user on 05.09.2022.
+//
+
+import Foundation
+
+
+struct Company: Codable {
+    
+    var count: Int?
+    var companyDescription: String?
+    var listOfData: [Device?]
+    
+}
+
+struct Device: Codable {
+    
+    var name: String?
+    var modelNumber: String?
+    var countries: [String?]
+    var price: [PriceInfo?]
+    var year: Int?
+}
+
+struct PriceInfo: Codable {
+    
+    var regions: [RegionPrice?]
+}
+
+
+struct RegionPrice: Codable {
+    var name: String?
+    var price: Int?
+}
