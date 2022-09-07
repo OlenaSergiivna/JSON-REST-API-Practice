cbdea56b65ba6b980d645add9649a693ce64b447
Olena <elenasergeevvnna@gmail.com>
2022-09-08 00:57:10 +0300

    Added configuring func.

--- JSON + REST API Practice/Views/MovieTableView/MovieTableViewCell.swift
+++ JSON + REST API Practice/Views/MovieTableView/MovieTableViewCell.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var movieTitle: UILabel!
+    
+    @IBOutlet weak var userScore: UILabel!
+    
+    @IBOutlet weak var movieType: UILabel!
+    
+    @IBOutlet weak var movieLanguage: UILabel!
+    
+    @IBOutlet weak var movieOverview: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         
     }
     
+    func configure(with data: Movie) {
+        
+        if let movieName = data.name {
+            movieTitle.text = movieName
+        } else {
+            movieTitle.text = data.title
+        }
+        
+        userScore.text = data.overview
+        movieType.text = data.mediaType
+        movieLanguage.text = data.originalLanguage
+        movieOverview.text = data.overview
+    }
 }
