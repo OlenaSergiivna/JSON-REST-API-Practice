eebd1c6b3d6751620a2cd036d2c16d0603be489a
Olena <elenasergeevvnna@gmail.com>
2022-09-16 19:17:34 +0300

    Added RealmModel.

--- JSON + REST API Practice/Models/Realm.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Realm.swift
-//  JSON + REST API Practice
-//
-//  Created by user on 16.09.2022.
-//
-
-import Foundation
---
+++ JSON + REST API Practice/Models/RealmModels.swift
@@ -0,0 +1,34 @@
+//
+//  Realm.swift
+//  JSON + REST API Practice
+//
+//  Created by user on 16.09.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class MoviesRealm: Object {
+    
+    @Persisted var adult: Bool
+    @Persisted var backdropPath: String?
+    @Persisted var id: Int
+    @Persisted var name: String?
+    @Persisted var originalLanguage: String
+    @Persisted var originalName: String?
+    @Persisted var overview: String?
+    @Persisted var posterPath: String?
+    @Persisted var mediaType: String
+    @Persisted var genreIDS: List<Int>
+    @Persisted var popularity: Double
+    @Persisted var firstAirDate: String?
+    @Persisted var voteAverage: Double
+    @Persisted var voteCount: Int
+    @Persisted var originCountry: List<String?>
+    @Persisted var title: String?
+    @Persisted var originalTitle: String?
+    @Persisted var releaseDate: String?
+    @Persisted var video: Bool?
+
+}
+
